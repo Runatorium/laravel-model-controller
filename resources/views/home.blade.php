43b9933d3e9aec3f3120ea900265cdc9e0a28439
Runatorium <stefano.alesci1@gmail.com>
2023-05-11 17:43:50 +0200

    bug da fixare

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,17 @@
 
 @section('content')
 
-    <h2>homepage</h2>
+    <div class="row">
+        @foreach ($Movies as $Movie)
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <p class="card-text">{{ $Movie->title }}</p>
+                        <p class="card-text">{{ $Movie->vote }}</p>
+                        <p class="card-text">{{ $Movie->date }}</p>
+                    </div>
+                </div>
+            </div>
+    </div>
 
 @endsection
